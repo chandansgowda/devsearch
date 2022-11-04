5d80b4fb926d284aed66e4c07347f7a60554c3a0
Chandan Gowda <chandansuresh007@gmail.com>
2022-11-04 20:51:41 +0530

    views, templates and admin

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,33 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community'
+    }
+]
+
+
+def projects(request):
+    return render(request,'projects/projects.html',{"projects": projectsList})
+
+def project(request,pk):
+    projectObject = None
+    for p in projectsList:
+        if p['id']==pk:
+            projectObject=p
+    return render(request,'projects/single-project.html',{'project':projectObject})
